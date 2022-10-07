bfb3737da4f51a4eb35da1f96e682ea03c50a65c
GUJJE AISHWARYA <96921172+aishwaryagit3851@users.noreply.github.com>
2022-10-07 14:27:28 +0530

    Update facultyreg.php

--- Task3/facultyreg.php
+++ Task3/facultyreg.php
@@ -155,7 +155,7 @@ if(isset($_POST['save'])){
     </div>
 </div>
 <div class="footer1">
-<a href="Result.html">Main Page</a>
+<a href="index.html">Main Page</a>
 </div>
 <script>
     function val(){
